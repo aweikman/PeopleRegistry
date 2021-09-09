36a0b0977b9893352db6118954a937df41ebd82f
Alan Weikman <alanweikman@gmail.com>
2021-09-08 21:00:02 -0500

    -fixed creating/updating save when adding or modifying person

--- src/main/java/mvc/screens/PersonDetailController.java
+++ src/main/java/mvc/screens/PersonDetailController.java
@@ -3,6 +3,7 @@ package mvc.screens;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import mvc.model.Person;
 import org.apache.logging.log4j.LogManager;
@@ -45,35 +46,61 @@ public class PersonDetailController implements Initializable {
 
     @FXML
     void save(ActionEvent event) {
-        if(person.getId() == 0) {
+        // temporary catch for empty fields to make sure user does not input empty lines
+        if(personId.getText().isEmpty() || personFirstName.getText().isEmpty() || personLastName.getText().isEmpty() || personDateofBirth.getText().isEmpty() || personAge.getText().isEmpty())
+        {
+            Alert fail= new Alert(Alert.AlertType.INFORMATION);
+            fail.setHeaderText("ERROR");
+            fail.setContentText("Please fill in all the fields");
+            fail.showAndWait();
+        }
+        else if(person.getId() == 0) {
             LOGGER.info("CREATING");
+            // TODO: validate text fields FIRST before you save them to model
+            person.setId(Integer.parseInt(personId.getText()));
+            person.setDateOfBirth(LocalDate.parse(personDateofBirth.getText()));
+            person.setPersonFirstName(personFirstName.getText());
+            person.setPersonLastName(personLastName.getText());
+            try {
+                person.setAge(Integer.parseInt(personAge.getText()));
+            } catch(NumberFormatException e) {
+                // TODO: find and plug in your alert helper functions
+                // Alert errorAlert = new Alert(Alert.AlertType.ERROR)
+                LOGGER.error("Hey man, person age must be an integer from 0 to 40");
+                return;
+            }
+            // transition to personlist
+            MainController.getInstance().switchView(ScreenType.PERSONLIST);
         }
         else
         {
             LOGGER.info("UPDATING");
+            // TODO: validate text fields FIRST before you save them to model
+            person.setId(Integer.parseInt(personId.getText()));
+            person.setDateOfBirth(LocalDate.parse(personDateofBirth.getText()));
+            person.setPersonFirstName(personFirstName.getText());
+            person.setPersonLastName(personLastName.getText());
+            try {
+                person.setAge(Integer.parseInt(personAge.getText()));
+            } catch(NumberFormatException e) {
+                // TODO: find and plug in your alert helper functions
+                // Alert errorAlert = new Alert(Alert.AlertType.ERROR)
+                LOGGER.error("Hey man, person age must be an integer from 0 to 40");
+                return;
+            }
+            // transition to personlist
+            MainController.getInstance().switchView(ScreenType.PERSONLIST);
         }
 
-        // TODO: validate text fields FIRST before you save them to model
-        person.setId(Integer.parseInt(personId.getText()));
-        person.setDateOfBirth(LocalDate.parse(personDateofBirth.getText()));
-        person.setPersonFirstName(personFirstName.getText());
-        person.setPersonLastName(personLastName.getText());
 
-        try {
-            person.setAge(Integer.parseInt(personAge.getText()));
-        } catch(NumberFormatException e) {
-            // TODO: find and plug in your alert helper functions
-            // Alert errorAlert = new Alert(Alert.AlertType.ERROR)
-            LOGGER.error("Hey man, person age must be an integer from 0 to 40");
-            return;
-        }
+
+
 
         // TODO: save the data to the database somewhere
         // TODO: what id db save fails?
         // ALERT
 
-        // transition to personlist
-        MainController.getInstance().switchView(ScreenType.PERSONLIST);
+
     }
 
     @Override
