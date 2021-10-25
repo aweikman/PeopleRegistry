9590314f31ac36aace9c910ba2124585db84967c
Alan Weikman <alanweikman@gmail.com>
2021-10-24 19:39:09 -0500

    added all return cases for response. good to go!

--- src/main/java/springboot/services/PeopleController.java
+++ src/main/java/springboot/services/PeopleController.java
@@ -11,6 +11,7 @@ import pw_hash.HashUtils;
 import springboot.model.People;
 import springboot.model.Users;
 import springboot.repository.PeopleRepository;
+import springboot.repository.SessionsRepository;
 import springboot.repository.UsersRepository;
 
 import java.text.ParseException;
@@ -25,23 +26,41 @@ import java.util.Optional;
 public class PeopleController {
     private static final Logger logger = LogManager.getLogger();
 
-
     private PeopleRepository peopleRepository;
+    private SessionsRepository sessionRepository;
 
-    public PeopleController(PeopleRepository peopleRepository) {
+    public PeopleController(PeopleRepository peopleRepository, SessionsRepository sessionRepository) {
         this.peopleRepository = peopleRepository;
+        this.sessionRepository = sessionRepository;
     }
 
 
     @GetMapping("/people")
-    public ResponseEntity<List<People>> fetchPeople(@RequestHeader Map<String, String> headers) {
+    public ResponseEntity<List<People>> fetchPeople(@RequestHeader HttpHeaders headers) {
+
+        // initialize token based on header
+        String sessionToken = headers.get("authorization").get(0);
+
+        // if token is not correct or not present return 401
+        if(sessionRepository.findById(sessionToken).isEmpty() || !headers.containsKey("authorization")){
+            return new ResponseEntity("Session token is invalid or missing.", HttpStatus.valueOf(401));
+        }
+
         // return 200 and fetch list of people from the db
         List<People> person = peopleRepository.findAll();
         return new ResponseEntity<>(person, HttpStatus.valueOf(200));
     }
 
     @DeleteMapping("/people/{id}")
-    public ResponseEntity<String> deletePerson(@PathVariable("id") int personId) {
+    public ResponseEntity<String> deletePerson(@PathVariable("id") int personId, @RequestHeader HttpHeaders headers) {
+
+        // initialize token based on header
+        String sessionToken = headers.get("authorization").get(0);
+
+        // if token is not correct or not present return 401
+        if(sessionRepository.findById(sessionToken).isEmpty() || !headers.containsKey("authorization")){
+            return new ResponseEntity("Session token is invalid or missing.", HttpStatus.valueOf(401));
+        }
 
         // check to see if id does not exist and return 404
         if(peopleRepository.findById(personId).isEmpty())
@@ -53,12 +72,21 @@ public class PeopleController {
         Optional<People> person = peopleRepository.findById(personId);
         peopleRepository.delete(person.get());
         return new ResponseEntity<>("Deleting person: " + personId, HttpStatus.valueOf(200));
-
     }
 
     @PostMapping("/people")
     @ResponseBody
-    public ResponseEntity<String> insertPerson(@RequestBody People person){
+    public ResponseEntity<String> insertPerson(@RequestBody People person, @RequestHeader HttpHeaders headers){
+
+        // initialize token based on header
+        String sessionToken = headers.get("authorization").get(0);
+
+        // if token is not correct or not present return 401
+        if(sessionRepository.findById(sessionToken).isEmpty() || !headers.containsKey("authorization")){
+            return new ResponseEntity("Session token is invalid or missing.", HttpStatus.valueOf(401));
+        }
+
+
         // code for checking if entered date is after current date
         Date date = null;
         try {
@@ -77,7 +105,7 @@ public class PeopleController {
         if(person.getFirstName().length() < 1 || person.getFirstName().length() > 100 ||
                 person.getLastName().length() < 1 || person.getLastName().length() > 100 || date.after(currentDate))
         {
-            return new ResponseEntity<>("Request body is incorrect (e.g., bad field name, validation error)", HttpStatus.valueOf(400));
+            return new ResponseEntity<>("Request body is incorrect (e.g., bad field name, validation error, please enter date of birth MM-dd-yyyy)", HttpStatus.valueOf(400));
         }
 
         // else we are returning 200 and adding person to table
@@ -87,7 +115,15 @@ public class PeopleController {
 
     @PutMapping("/people/{id}")
     @ResponseBody
-    public ResponseEntity<String> updatePerson(@PathVariable("id") int personId, @RequestBody People person) {
+    public ResponseEntity<String> updatePerson(@PathVariable("id") int personId, @RequestBody People person, @RequestHeader HttpHeaders headers) {
+
+        // initialize token based on header
+        String sessionToken = headers.get("authorization").get(0);
+
+        // if token is not correct or not present return 401
+        if(sessionRepository.findById(sessionToken).isEmpty() || !headers.containsKey("authorization")){
+            return new ResponseEntity("Session token is invalid or missing.", HttpStatus.valueOf(401));
+        }
 
         // code for checking if entered date is after current date
         Date date = null;
@@ -107,7 +143,7 @@ public class PeopleController {
         if(person.getFirstName().length() < 1 || person.getFirstName().length() > 100 ||
                 person.getLastName().length() < 1 || person.getLastName().length() > 100 || date.after(currentDate))
         {
-            return new ResponseEntity<>("Request body is incorrect (e.g., bad field name, validation error)", HttpStatus.valueOf(400));
+            return new ResponseEntity<>("Request body is incorrect (e.g., bad field name, validation error, please enter date of birth MM-dd-yyyy)", HttpStatus.valueOf(400));
         }
 
         // check to see if id does not exist and return 404
@@ -117,6 +153,7 @@ public class PeopleController {
         }
 
         // else we are returning 200 and updating table
+
         // we don't expect the incoming json to have the widget's id, so we set it
         person.setId(personId);
 
@@ -129,19 +166,22 @@ public class PeopleController {
 
     @GetMapping("/people/{id}")
     @ResponseBody
-    public ResponseEntity <String> fetchPerson(@RequestHeader Map<String, String> headers, @PathVariable("id") int personId) {
+    public ResponseEntity <String> fetchPerson(@RequestHeader HttpHeaders headers, @PathVariable("id") int personId) {
         Optional <People> person = peopleRepository.findById(personId);
 
-        // if person exists fetch info and put into response body and return 200
-        if(person.isPresent()) {
-            return new ResponseEntity<>("Fetching Person with ID: " + personId + " " + peopleRepository.findById(personId),  HttpStatus.valueOf(200));
-        }
+        // initialize token based on header
+        String sessionToken = headers.get("authorization").get(0);
+
         // if person does not exist based on id return 404
-        else if (peopleRepository.findById(personId).isEmpty()){
+        if (peopleRepository.findById(personId).isEmpty()){
             return new ResponseEntity<>(null, HttpStatus.valueOf(404));
         }
-        else {
-            return new ResponseEntity<>(null, HttpStatus.valueOf(401));
+        // if token is not correct or not present return 401
+        else if(sessionRepository.findById(sessionToken).isEmpty() || !headers.containsKey("authorization")){
+            return new ResponseEntity("Session token is invalid or missing.", HttpStatus.valueOf(401));
         }
+
+        // if person exists fetch info and put into response body and return 200
+        return new ResponseEntity<>("Fetching Person with ID: " + personId + " " + peopleRepository.findById(personId),  HttpStatus.valueOf(200));
     }
 }
